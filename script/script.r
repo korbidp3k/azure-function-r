a067f1b8335c43dc3432371d3dd42d59475cbf1e
Adrianto <adrianto@immunoSCAPEs-MacBook-Pro.local>
2017-08-11 14:39:11 +0800

    updated functions

--- script/script.r
+++ script/script.r
@@ -1,1 +1,8 @@
- print(paste("Hello, world!"))
+if (!require(flowCore)) {
+  source("http://bioconductor.org/biocLite.R")
+  biocLite("flowCore")
+}
+
+for (idx in 1:1000){
+ print(paste(idx, "Hello, world!", sep = " "))
+}
